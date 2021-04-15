eadaf6ec154fd3d1e0075c4c0079debda0220c2b
myzou <944711140@qq.com>
2021-04-15 13:58:04 +0800

    2021.04.15 修改mtp 新的单子修改为未mtp

--- src/com/cter/dao/impl/MTPProvisionDaoImpl.java
+++ src/com/cter/dao/impl/MTPProvisionDaoImpl.java
@@ -103,7 +103,7 @@ public class MTPProvisionDaoImpl extends BaseDaOImpl<MTPProvision> {
     public int updateData(MTPProvision mtpProvision) {
         String sql = "INSERT INTO `mtp`.`mtp_provision`(`case_id`, `jsonstr`, `start_time`, `end_time`, `run_before`, `run_after`, `runing`, `update_time`)" +
                 " VALUES (?,?,?,?,'0','0','0',now()) " +
-                "ON DUPLICATE KEY UPDATE jsonstr=?,start_time=?,end_time=?,update_time=now()";
+                "ON DUPLICATE KEY UPDATE jsonstr=?,start_time=?,end_time=?,update_time=now(),run_before=0,run_after=0";
         String username = "root";
         String password = "root1234root";
         String url = "jdbc:log4jdbc:mysql://210.5.3.30:3306/mtp?characterEncoding=utf-8";
